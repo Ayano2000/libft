d50340d55a9782975bd2be63f903a4535d271227
Event WeThinkCode <event@c3r3s2.wethinkcode.co.za>
2019-05-20 12:50:02 +0200

    finished skeleton code of part 1 - makefile

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+ char   *strrchr(const char *str, int to_find)
+ {
+     size_t i;
+
+     i = ft_strlen(str);
+     while(str[i] != to_find && 0 != i)
+        i--;
+    if (0 == i)
+        return (NULL);
+    else
+        return ((char *) to_find);
+ }
--- libft/libft.h
+++ libft/libft.h
@@ -33,7 +33,9 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strncat(char *str1, const char *str2, size_t n);
-
-
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*strrchr(const char *str, int to_find);
+void    *ft_memccpy(void *dst, const void *src,int c, size_t n);
+char	*ft_strcpy(char *dest, const char *src);
 
 #endif
