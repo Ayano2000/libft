3d29702dcb9ba18ac2a13369be14658ab21c04cd
Arata YANO <ayano@c3r5s9.wethinkcode.co.za>
2019-05-29 08:28:31 +0200

    fixed fringe tests for putnbr, putnbr_fd, strequ and strnequ

--- libft/ft_strequ.c
+++ libft/ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: ayano <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 16:01:13 by ayano             #+#    #+#             */
-/*   Updated: 2019/05/27 16:02:51 by ayano            ###   ########.fr       */
+/*   Updated: 2019/05/29 07:46:05 by ayano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,14 @@ int		ft_strequ(char const *str1, char const *str2)
 	i = 0;
 	if (str1 == NULL || str2 == NULL)
 		return (0);
+	if (ft_strlen(str1) != ft_strlen(str2))
+		return (0);
 	while (str1[i] != '\0' && str2[i] != '\0')
 	{
 		if (str1[i] != str2[i])
 			return (0);
-		i++;
+		else
+			i++;
 	}
 	return (1);
 }
